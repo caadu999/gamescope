a4814a35d7919b2d701a1c9342cfd5b47a786b35
Cadu <cadums013@gmail.com>
2026-07-23 19:45:02 -0300

    feat: adiciona transição entre páginas

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -6,10 +6,13 @@ import CardFlutuante from '../cardFlutuante/cardFlutuante';
 import { FaFireAlt } from 'react-icons/fa';
 import { BiSolidGame } from 'react-icons/bi';
 import SearchInput from '../searchInput';
+import Transicao from '@/components/Transicao';
 
 export default function Hero() {
   return (
     <section className={styles.container}>
+      <Transicao />
+
       <TituloHome text="Ache seu novo jogo favorito aqui." />
       <CardFlutuante className="left-2/4 top-8 z-30 hidden rounded-[50%] bg-[#ff6d38] p-3 md:absolute md:hidden lg:block">
         <div>
