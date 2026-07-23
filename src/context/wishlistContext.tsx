d3d46e295c4f3f3225d115e9822e3756914c4719
Cadu <cadums013@gmail.com>
2026-07-23 16:24:31 -0300

    fix: correção de erro eslint

--- src/context/wishlistContext.tsx
+++ src/context/wishlistContext.tsx
@@ -24,6 +24,7 @@ export function WishlistProvider({ children }: WishlistProviderProps) {
   // roda só no cliente
   useEffect(() => {
     const item = getItem('wishlist');
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     if (item) setWishlist(item as Results[]);
   }, []);
 
